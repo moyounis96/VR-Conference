f79abc2a6d1506dd67739529e4e81b297220374a
AQaddora <hem-plo@hotmail.com>
2021-06-06 18:55:45 +0400

    pure android version

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,10 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Loader : MonoBehaviour
 {
+    public VideoClip[] clips;
+    public VideoPlayer player;
+
+    public GameObject[] buttons;
+
     public void LoadScene(int index)
     {
         SceneManager.LoadScene(index);
+    }
+    public void PlayVideo(int index)
+    {
+        foreach (GameObject gameObject in buttons)
+        {
+            gameObject.SetActive(false);
+        }
+        player.gameObject.SetActive(true);
+        player.Stop();
+        player.clip = clips[index];
+        player.Play();
+    }
+
+    public void StopVideo()
+    {
+        player.Stop();
+        foreach (GameObject gameObject in buttons)
+        {
+            gameObject.SetActive(true);
+        }
+        player.gameObject.SetActive(false);
     }
 }
